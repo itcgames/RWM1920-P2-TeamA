cee501cf52f6554056df6a1e6aa92e5f5033fcdb
OisinWilson <oisinwilson@gmail.com>
2019-11-26 17:43:06 +0000

    basic player + jump added

---
+++ ProjectGizmo/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool movementDirection = false;
+    public float speed = 0;
+    public float jumpHeight = 0;
+
+    private bool jump = false;
+    private float jumpspeed = 0f;
+
+    private float time = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (jump == false)
+            {
+                jump = true;
+                StartCoroutine(jumpWait());
+                jumpspeed = jumpHeight;
+            }
+        }
+
+        if (jump)
+        { 
+            rb.position += movementDirection ? new Vector2(speed, jumpspeed) : new Vector2(-speed, jumpspeed);
+            jumpspeed *= 0.9f;
+            if (jumpspeed <= 0)
+            {
+                jumpspeed = 0;
+            }
+        }
+        else
+        {
+            rb.position += movementDirection ? new Vector2(speed, 0) : new Vector2(-speed, 0);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag != "Ground")
+        {
+            var direction = transform.InverseTransformPoint(col.transform.position);
+
+            if (direction.x > 0f || direction.x < 0f)
+            {
+                movementDirection = !movementDirection;
+            }
+        }
+    }
+
+
+    IEnumerator jumpWait()
+    {
+        yield return new WaitForSeconds(time);
+        jump = false;
+    }
+}
