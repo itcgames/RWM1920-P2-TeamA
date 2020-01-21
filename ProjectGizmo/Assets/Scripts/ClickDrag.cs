4efa72b9782748047fb991633703940a57de7dd2
AaronODea <c00217820@itcarlow.ie>
2020-01-21 15:04:09 +0000

    drag and drop tracking object

--- ProjectGizmo/Assets/Scripts/ClickDrag.cs
+++ ProjectGizmo/Assets/Scripts/ClickDrag.cs
@@ -17,6 +17,8 @@ public class ClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     private Vector3 ImageStartPosition;
     public RectTransform pos;
     public GameObject Item;
+    private GameObject ItemDrag;
+
 
 
     private void Start()
@@ -27,21 +29,24 @@ public class ClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         if (dragging)
         {
-            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            //   Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            ItemDrag.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+           // Debug.Log(transform.position);
+
         }
     }
     public void OnPointerUp(PointerEventData eventData)
     {
         dragging = false;
-        transform.position = ImageStartPosition;
-       
+      //  transform.position = ImageStartPosition;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         dragging = true;
         Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Instantiate(Item, new Vector2((clickedPosition.x), (clickedPosition.y)), Quaternion.identity);
+        ItemDrag = Instantiate(Item, new Vector2((clickedPosition.x), (clickedPosition.y)), Quaternion.identity);
 
     }
 }
