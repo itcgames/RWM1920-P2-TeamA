f999923e51747a5cf435ab40d0a3e0f3b8e566e9
AaronODea <c00217820@itcarlow.ie>
2020-01-22 11:46:24 +0000

    update fixes and drag

--- ProjectGizmo/Assets/Scripts/ClickDrag.cs
+++ ProjectGizmo/Assets/Scripts/ClickDrag.cs
@@ -14,39 +14,52 @@ using UnityEngine.UI;
 public class ClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     private bool dragging;
-    private Vector3 ImageStartPosition;
     public RectTransform pos;
     public GameObject Item;
     private GameObject ItemDrag;
 
+    private bool playing = false;
+
+
 
 
     private void Start()
-    {
-        ImageStartPosition = pos.position;
+    {     
+        
+       
     }
     public void Update()
     {
+
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("GizmoSprite");
+
         if (dragging)
         {
-            //   Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
             ItemDrag.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
-           // Debug.Log(transform.position);
-
         }
+
+        if(objects.Length > 0)
+        {
+            playing = true;
+        }
+        else
+        {
+            playing = false;
+        }
+
     }
     public void OnPointerUp(PointerEventData eventData)
     {
         dragging = false;
-      //  transform.position = ImageStartPosition;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        dragging = true;
-        Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        ItemDrag = Instantiate(Item, new Vector2((clickedPosition.x), (clickedPosition.y)), Quaternion.identity);
-
+        if (playing == true)
+        {
+            dragging = true;
+            Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ItemDrag = Instantiate(Item, new Vector2((clickedPosition.x), (clickedPosition.y)), Quaternion.identity);
+        }
     }
 }
