bdebab44f408b69b85ad06d4f407ed20224dece9
AaronODea <c00217820@itcarlow.ie>
2020-01-22 14:20:48 +0000

    sub modules all uploaded

---
+++ ProjectGizmo/Assets/Plugins/submodules/RWM1920-C00217820/Fan/Assets/Scripts/DragFan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFan : MonoBehaviour
+{
+    private float m_coordinates;
+    private Vector3 m_Offset;
+   
+
+    void OnMouseDown()
+    {
+        m_coordinates = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        m_Offset = gameObject.transform.position - GetMouseAsWorldPoint();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseAsWorldPoint() + m_Offset;
+    }
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = m_coordinates;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+
+}
+
